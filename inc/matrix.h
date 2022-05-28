0ef6696fe2266c433c02b944540ed35b435b943b
AlKl1M <al.klimkovetskiy@gmail.com>
2022-05-28 19:15:20 +0200

    inverse matrix done

--- inc/matrix.h
+++ inc/matrix.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
 
 namespace mt {
 	template <class T>
@@ -10,6 +11,7 @@ namespace mt {
 		Matrix(int Row, int Col);
 		Matrix(int Row, int Col, const T* Data);
 		Matrix(const Matrix<T>& matrix);
+		Matrix(int Row, int Col, const std::vector<T>* Data);
 
 		~Matrix();
 
@@ -34,8 +36,22 @@ namespace mt {
 		Matrix<T> FindSubMatrix(int Row, int Col);
 		T Determinant();
 		Matrix<T> transposition();
+		bool Resize(int numRows, int numCols);
+		bool Inverse();
+		bool Separate(Matrix<T>& matrix1, Matrix<T>& matrix2, int colNum);
 	private:
-		
+		bool IsSquare();
+		bool CloseEnough(T f, T g);
+		void SwapRow(int i, int j);
+		void MultAdd(int i, int j, T multFactor); // j * multFactor + i
+		void MultRow(int i, T multFactor);  // i * multFactor
+		bool Join(const Matrix<T>& matrix2); // concatination
+		int Sub2Ind(int row, int col) const;
+		int FindRowWithMaxElement(int colNumber, int startingRow); // from starting row in colNumber it search max
+		void PrintMatrix();
+		void SetToIdentity();
+
+	private:
 		T* m_matrixData;
 		int m_Row, m_Col, m_Elements;
 	};
@@ -87,6 +103,17 @@ namespace mt {
 			m_matrixData[i] = matrix.m_matrixData[i];
 	}
 
+	template <class T>
+	Matrix<T>::Matrix(int Row, int Col, const std::vector<T>* Data)
+	{
+		m_Row = Row;
+		m_Col = Col;
+		m_Elements = m_Row * m_Col;
+		m_matrixData = new T[m_Elements];
+		for (int i = 0; i < m_Elements; i++)
+			m_matrixData[i] = Data->at(i);
+	}
+
 	// Destructor
 	template <class T>
 	Matrix<T>::~Matrix()
@@ -101,12 +128,7 @@ namespace mt {
 	template <class T>
 	T Matrix<T>::getElement(int Row, int Col) const
 	{
-		int l = 0;
-		if ((Row < m_Row) && (Row >= 0) && (Col < m_Col) && (Col >= 0))
-			l = (Row * m_Col) + Col;
-		else
-			l = -1;
-
+		int l = Sub2Ind(Row, Col);
 		if (l >= 0)
 			return m_matrixData[l];
 		else
@@ -115,12 +137,10 @@ namespace mt {
 
 	// Setter
 	template <class T>
-	bool Matrix<T>::SetElement(int Row, int Col, T value) {
-		int l = 0;
-		if ((Row < m_Row) && (Row >= 0) && (Col < m_Col) && (Col >= 0))
-			l = (Row * m_Col) + Col;
-		else
-			l = -1;
+	bool Matrix<T>::SetElement(int Row, int Col, T value) 
+	{
+		int l = Sub2Ind(Row, Col);
+
 		if (l >= 0) {
 			m_matrixData[l] = value;
 			return true;
@@ -364,5 +384,277 @@ namespace mt {
 			}
 		}
 		return temp;
+	}
+
+	template <class T>
+	bool Matrix<T>::CloseEnough(T f, T g)
+	{
+		return fabs(f - g) < 1e-9; // how close to 0
+	}
+
+	template <class T>
+	bool Matrix<T>::Resize(int numRows, int numCols)
+	{
+		m_Row = numRows;
+		m_Col = numCols;
+		m_Elements = (m_Row * m_Col);
+		delete[] m_matrixData;
+		m_matrixData = new T[m_Elements];
+		if (m_matrixData != nullptr)
+		{
+			for (int i = 0; i < m_Elements; i++) {
+				m_matrixData[i] = 0.0;
+			}
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	// separate matrix to 2 matrices from nCol
+	template <class T>
+	bool Matrix<T>::Separate(Matrix<T>& matrix1, Matrix<T>& matrix2, int nCol)
+	{
+		int nRow = m_Row;
+		int nCol1 = nCol;
+		int nCol2 = m_Col - nCol;
+
+		matrix1.Resize(nRow, nCol1);
+		matrix2.Resize(nRow, nCol1);
+
+		for (int i = 0; i < m_Row; i++)
+		{
+			for (int j = 0; j < m_Col; j++)
+			{
+				if (j < nCol)
+				{
+					matrix1.SetElement(i, j, this->getElement(i, j));
+				}
+				else
+				{
+					matrix2.SetElement(i, j - nCol, this->getElement(i, j));
+				}
+			}
+			return true;
+		}
+	}
+
+	template <class T>
+	bool Matrix<T>::Join(const Matrix<T>& matrix2)
+	{
+		int n_Row1 = m_Row;
+		int n_Row2 = matrix2.m_Row;
+		int n_Col1 = m_Col;
+		int n_Col2 = matrix2.m_Col;
+
+		if (n_Row1 != n_Row2)
+			throw std::invalid_argument("You can not join matrices with different numbers of rows");
+
+		// concatinate data
+		T* newData = new T[n_Row1 * (n_Col1 + n_Col2)];
+
+		int index, resultIndex;
+		for (int i = 0; i < n_Row1; i++)
+		{
+			for (int j = 0; j < (n_Col1 + n_Col2); j++)
+			{
+				resultIndex = (i * (n_Col1 + n_Col2)) + j;
+
+				if (j < n_Col1)
+				{
+					index = (i * n_Col1) + j;
+					newData[resultIndex] = m_matrixData[index];
+				}
+				else
+				{
+					index = (i * n_Col2) + (j - n_Col1);
+					newData[resultIndex] = matrix2.m_matrixData[index];
+				}
+			}
+		}
+		m_Col = n_Col1 + n_Col2;
+		m_Elements = m_Row * m_Col;
+		delete[] m_matrixData;
+		m_matrixData = new T[m_Elements];
+		for (int i = 0; i < m_Elements; i++)
+			m_matrixData[i] = newData[i];
+		
+		delete[] newData;
+		return true;
+	}
+
+	// method to get right RxC position of element from array
+	template <class T>
+	int Matrix<T>::Sub2Ind(int row, int col) const
+	{
+		if ((row < m_Row) && (row >= 0) && (col < m_Col) && (col >= 0))
+			return (row * m_Col) + col;
+		else
+			return -1;
+	}
+
+	template <class T>
+	void Matrix<T>::SwapRow(int i, int j)
+	{
+		T* tempR = new T[m_Col];
+		for (int k = 0; k < m_Col; k++)
+			tempR[k] = m_matrixData[Sub2Ind(i, k)];
+
+		for (int k = 0; k < m_Col; k++)
+			m_matrixData[Sub2Ind(i, k)] = m_matrixData[Sub2Ind(j, k)];
+
+		for (int k = 0; k < m_Col; k++)
+			m_matrixData[Sub2Ind(j, k)] = tempR[k];
+
+		delete[] tempR;
+	}
+
+	template <class T>
+	void Matrix<T>::MultAdd(int i, int j, T multFactor)
+	{
+		for (int k = 0; k < m_Col; k++)
+			m_matrixData[Sub2Ind(i, k)] += (m_matrixData[Sub2Ind(j, k)] * multFactor);
+	}
+
+	template <class T>
+	int Matrix<T>::FindRowWithMaxElement(int colNumber, int startingRow)
+	{
+		T temp = m_matrixData[Sub2Ind(startingRow, colNumber)];
+		int rowIndex = startingRow;
+		for (int i = startingRow + 1; i < m_Row; i++)
+		{
+			if (fabs(m_matrixData[Sub2Ind(i, colNumber)]) > fabs(temp))
+			{
+				rowIndex = i;
+				temp = m_matrixData[Sub2Ind(i, colNumber)];
+			}
+		}
+		return rowIndex;
+	}
+
+	template <class T>
+	void Matrix<T>::MultRow(int i, T multFactor)
+	{
+		for (int k = 0; k < m_Col; k++)
+			m_matrixData[Sub2Ind(i, k)] *= multFactor;
+	}
+
+	template <class T>
+	bool Matrix<T>::IsSquare()
+	{
+		if (m_Col == m_Row)
+			return true;
+		else
+			return false;
+	}
+
+	// method to make any matrix identity
+	template <class T>
+	void Matrix<T>::SetToIdentity()
+	{
+		if (!IsSquare())
+			throw std::invalid_argument("Matrix is not square");
+
+		for (int i = 0; i < m_Row; i++)
+		{
+			for (int j = 0; j < m_Col; j++)
+			{
+				if (i == j)
+					m_matrixData[Sub2Ind(i, j)] = 1.0;
+				else
+					m_matrixData[Sub2Ind(i, j)] = 0.0;
+			}
+		}
+	}
+
+	template <class T>
+	bool Matrix<T>::Inverse()
+	{
+		if (!IsSquare())
+			throw std::invalid_argument("Operation does not support matrix that doesnt square");
+
+		Matrix<T> identityMatrix(m_Row, m_Col);
+		identityMatrix.SetToIdentity();
+		int originalNumCols = m_Col;
+		// if matrix is square then we concat it with identity matrix
+		Join(identityMatrix);
+		int cRow, cCol;
+		int maxCount = 100;
+		int count = 0;
+		bool flag = false;
+		while ((!flag) && (count < maxCount))
+		{
+			for (int i = 0; i < m_Row; i++)
+			{
+				cRow = i;
+				cCol = i;
+
+				int maxIndex = FindRowWithMaxElement(cCol, cRow); // next we finding row with max element in cCol
+
+				if (maxIndex != cRow) // if that doesnt current row we swap them
+				{
+					SwapRow(cRow, maxIndex);
+				}
+
+				if (m_matrixData[Sub2Ind(cRow, cCol)] != 1.0)
+				{
+					T mult = 1.0 / m_matrixData[Sub2Ind(cRow, cCol)]; // 1.0 / currentElement
+					MultRow(cRow, mult);
+				}
+
+				// going though all rows of this column
+				for (int j = cRow + 1; j < m_Row; j++)
+				{
+					if (!CloseEnough(m_matrixData[Sub2Ind(j, cCol)], 0.0))
+					{
+						int rowOneIndex = cCol;
+						// value at the current element
+						T currentElementValue = m_matrixData[Sub2Ind(j, cCol)];
+						// get the value at (rowOneIndex and cCol)
+						T rowOneValue = m_matrixData[Sub2Ind(rowOneIndex, cCol)];
+
+						// if that close to zero then we move
+						if (!CloseEnough(rowOneValue, 0.0))
+						{
+							T correctionFactor = -(currentElementValue / rowOneValue);
+							MultAdd(j, rowOneIndex, correctionFactor); // rowOneIndex * cF + j
+						}
+					}
+				}
+				for (int k = cCol + 1; k < originalNumCols; k++)
+				{
+					if (!CloseEnough(m_matrixData[Sub2Ind(cRow, k)], 0.0))
+					{
+						int rowOneIndex = k;
+						T currentElementValue = m_matrixData[Sub2Ind(cRow, k)];
+						T rowOneValue = m_matrixData[Sub2Ind(rowOneIndex, k)];
+						if (!CloseEnough(rowOneValue, 0.0))
+						{
+							T correctionFactor = -(currentElementValue / rowOneValue);
+							MultAdd(cRow, rowOneIndex, correctionFactor);
+						}
+					}
+				}
+			}
+			Matrix<T> left;
+			Matrix<T> right;
+			this->Separate(left, right, originalNumCols);
+
+			if (left == identityMatrix)
+			{
+				flag = true;
+
+				m_Col = originalNumCols;
+				m_Elements = m_Row * m_Col;
+				delete[] m_matrixData;
+				m_matrixData = new T[m_Elements];
+				for (int i = 0; i < m_Elements; i++)
+					m_matrixData[i] = right.m_matrixData[i];
+			}
+			count++;
+		}
+		return flag;
 	}
 } 
